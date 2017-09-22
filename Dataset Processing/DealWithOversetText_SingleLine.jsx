459909acfc823545aeb1c4cb6e2722de8794f934
Silly-V <vasily.hall@gmail.com>
2017-09-22 13:24:34 -0500

    update: works on tags instead of notes

--- Dataset Processing/DealWithOversetText_SingleLine.jsx
+++ Dataset Processing/DealWithOversetText_SingleLine.jsx
@@ -1,92 +1,124 @@
-#target illustrator
-
 function DealWithOversetText_SingleLine() {
-    function recordFontSize() {
-        for (var i = 0; i < doc.textFrames.length; i++) {
-            var t = doc.textFrames[i];
-            if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
-                t.note = t.textRange.characterAttributes.size;
-            }
-        };
-    }
+  var defaultSizeTagName = "overset_text_default_size";
 
-    function removeNotesOnText() {
-        for (var i = 0; i < doc.textFrames.length; i++) {
-            var t = doc.textFrames[i];
-            t.note = "";
+  function recordFontSizeInTag(size, art){
+    var tag;
+    var tags = art.tags;
+    try {
+      tag = tags.getByName(defaultSizeTagName);
+      tag.value = size;
+    } catch(e) {
+      tag = tags.add();
+      tag.name = defaultSizeTagName;
+      tag.value = size;
+    }
+  }
+
+  function readFontSizeFromTag(art){
+    var tag;
+    var tags = art.tags;
+    try {
+      tag = tags.getByName(defaultSizeTagName);
+      return tag.value * 1;
+    } catch(e) {
+      return null;
+    }
+  }
+
+  function recordFontSize() {
+    var doc = app.activeDocument;
+    for (var i = 0; i < doc.textFrames.length; i++) {
+      var t = doc.textFrames[i];
+      if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
+        // t.note = t.textRange.characterAttributes.size;
+        recordFontSizeInTag(t.textRange.characterAttributes.size, t);
+      }
+    };
+  }
+
+  function isOverset(textBox) {
+    if (textBox.lines.length > 0) {
+      if (textBox.lines[0].characters.length < textBox.characters.length) {
+        return true;
+      } else {
+        return false;
+      }
+    } else if (textBox.characters.length > 0) {
+      return true;
+    }
+  }
+
+  function shrinkFont(textBox) {
+    var totalCharCount = textBox.characters.length;
+    var lineCount = textBox.lines.length;
+    if (textBox.lines.length > 0) {
+      var firstLineCharCount = textBox.lines[0].characters.length;
+      if (isOverset(textBox)) {
+        var inc = 0.01;
+        while (isOverset(textBox)) {
+          textBox.textRange.characterAttributes.size -= inc;
         }
+      }
+    } else if (textBox.characters.length > 0) {
+      var inc = 0.01;
+      while (isOverset(textBox)) {
+        textBox.textRange.characterAttributes.size -= inc;
+      }
     }
+  }
 
-    function isOverset(textBox) {
-        if (textBox.lines.length > 0) {
-            if (textBox.lines[0].characters.length < textBox.characters.length) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (textBox.characters.length > 0) {
-            return true;
-        }
+  function resetSize(textAreaBox) {
+    var t = textAreaBox;
+    if (t.contents != "") {
+      // if (t.note != "") {
+      //     t.textRange.characterAttributes.size = (t.note * 1);
+      // }
+      var size = readFontSizeFromTag(t);
+      if(size != null){
+        t.textRange.characterAttributes.size = size;
+      }
     }
+  }
 
-    function shrinkFont(textBox) {
-        var totalCharCount = textBox.characters.length;
-        var lineCount = textBox.lines.length;
-        if (textBox.lines.length > 0) {
-            var firstLineCharCount = textBox.lines[0].characters.length;
-            if (isOverset(textBox)) {
-                var inc = 0.01;
-                while (isOverset(textBox)) {
-                    textBox.textRange.characterAttributes.size -= inc;
-                }
-            }
-        } else if (textBox.characters.length > 0) {
-            var inc = 0.01;
-            while (isOverset(textBox)) {
-                textBox.textRange.characterAttributes.size -= inc;
-            }
-        }
+  function removeTagsOnText(){
+    var doc = app.activeDocument;
+    for (var i = 0; i < doc.textFrames.length; i++) {
+      try {
+        doc.textFrames[i].tags.getByName(defaultSizeTagName).remove();
+      } catch (e) {
+
+      }
     }
+  }
 
-    function resetSize(textAreaBox) {
-        var t = textAreaBox;
-        if (t.contents != "") {
-            if (t.note != "") {
-                t.textRange.characterAttributes.size = (t.note * 1);
-            }
-        }
+  function resetAllTextBoxes() {
+    for (var i = 0; i < doc.textFrames.length; i++) {
+      var t = doc.textFrames[i];
+      if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
+        resetSize(t);
+      }
+    };
+  }
+
+  function shrinkAllTextBoxes() {
+    for (var i = 0; i < doc.textFrames.length; i++) {
+      var t = doc.textFrames[i];
+      if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
+        shrinkFont(t);
+      }
+    };
+  }
+  if (app.documents.length > 0) {
+    var doc = app.activeDocument;
+    if (doc.activeDataSet == doc.dataSets[0]) {
+      recordFontSize();
     }
-
-    function resetAllTextBoxes() {
-        for (var i = 0; i < doc.textFrames.length; i++) {
-            var t = doc.textFrames[i];
-            if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
-                resetSize(t);
-            }
-        };
+    resetAllTextBoxes();
+    shrinkAllTextBoxes();
+    if (doc.activeDataSet == doc.dataSets[doc.dataSets.length - 1]) {
+      removeTagsOnText();
     }
-
-    function shrinkAllTextBoxes() {
-        for (var i = 0; i < doc.textFrames.length; i++) {
-            var t = doc.textFrames[i];
-            if (t.kind == TextType.AREATEXT && t.editable && !t.locked && !t.hidden) {
-                shrinkFont(t);
-            }
-        };
-    }
-
-    if (app.documents.length > 0) {
-        var doc = app.activeDocument;
-        if (doc.activeDataSet == doc.dataSets[0]) {
-            recordFontSize();
-        }
-
-        resetAllTextBoxes();
-        shrinkAllTextBoxes();
-
-        if (doc.activeDataSet == doc.dataSets[doc.dataSets.length - 1]) {
-            removeNotesOnText();
-        }
-    }
+  }
+  return true;
 }
 DealWithOversetText_SingleLine();
